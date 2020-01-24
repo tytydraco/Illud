6fe5dd4f428e0269a7dbbf957fc8ea0863c0e8dd
tytydraco <tylernij@gmail.com>
2020-01-23 17:02:51 -0800

    Better document global file
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/illud/utils/Global.kt
+++ app/src/main/java/com/draco/illud/utils/Global.kt
@@ -1,10 +1,10 @@
 package com.draco.illud.utils
 
 /*
- * This class just holds some declarations that are accessed in
- * most files, so it makes the most sense to just globalize them
- * instead of pass each one via reference to each class.
+ * This file just holds some declarations that are accessed in
+ * most files, so it makes sense to just globalize them instead
+ * of pass each one via reference to each class.
  */
 
-/* Our global copy of classes */
+/* Our global copies of classes */
 val listItems = ListItems()
